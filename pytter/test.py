e463555b02f1a3af48fccd0e24e24394e8b4f9d5
Vincent Loy <vincent.loy1@gmail.com>
2015-07-25 16:39:48 +0200

    add empty unit test

---
+++ pytter/test.py
@@ -0,0 +1,16 @@
+# vim: fileencoding=utf-8 tw=100 expandtab ts=4 sw=4 :
+#
+# pytter
+# copyright (c) 2015 Vincent Loy
+# Vincent Loy <vincent.loy1@gmail.com>
+
+import unittest
+
+
+class PytterTest(unittest.TestCase):
+    def test_something(self):
+        self.assertEqual(True, False)
+
+
+if __name__ == '__main__':
+    unittest.main()
